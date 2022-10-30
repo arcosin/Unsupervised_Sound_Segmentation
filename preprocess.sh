c1628ff1eaf82a5d433d58e4fdd5f355ca448961
Ziyang Huang <huan1562@purdue.edu>
2022-10-30 13:53:29 -0400

    Fix preprocess dir error

--- preprocess.sh
+++ preprocess.sh
@@ -2,3 +2,13 @@ python src/preprocess.py \
     --in_dir 'dataset/raw/' \
     --out_dir 'dataset/processed/' \
     --chunk_time 5
+
+# list all folders in the processed folder except spectrogram
+for folder in $(ls -d dataset/processed/* | grep -v spectrogram); do
+    for wav in $(ls $folder/*.wav); do
+        # echo $folder/spectrogram
+        python src/wav_to_spectrogram.py \
+            --in_file $wav \
+            --out_dir $folder/spectrogram/
+    done
+done
--- src/wav_to_spectrogram.py
+++ src/wav_to_spectrogram.py
@@ -1,6 +1,6 @@
 
 
-# Usage: python wav_to_spectrogram.py --in_file <input_file> --out_file <output_file>
+# Usage: python wav_to_spectrogram.py --in_file <input_file> --out_dir <output_dir>
 
 import argparse
 import os
@@ -8,6 +8,7 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
+from utils import print_yellow, print_red, print_green
 
 
 def wav_to_spectrogram(in_file: str, out_dir: str) -> None:
@@ -18,16 +19,19 @@ def wav_to_spectrogram(in_file: str, out_dir: str) -> None:
     data = librosa.to_mono(data)
     # Compute the spectrogram
     S = librosa.feature.melspectrogram(
-        data, sr=sr, hop_length=1024, n_fft=2048, n_mels=128)
+        y=data, sr=sr, hop_length=1024, n_fft=2048, n_mels=128)
     # Convert to log scale
     log_S = librosa.power_to_db(S, ref=np.max)
     # Plot the spectrogram
-    fig = plt.figure(figsize=(128, 128))
+    fig = plt.figure(figsize=(500, 128))
     librosa.display.specshow(log_S, sr=sr)
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.axis('off')
     # Save the spectrogram
-    out_file = os.path.join(out_dir, os.path.basename(in_file) + '.png')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    out_file = out_dir + in_file.split('/')[-1][:-4] + '.png'
+    print_green(f'Saving {out_file}')
     plt.savefig(out_file, bbox_inches='tight', pad_inches=0)
     plt.close()
 
