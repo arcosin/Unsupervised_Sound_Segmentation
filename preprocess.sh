4f87deba6f6e9e58b0516056b9f13a9611cb4a0c
Ziyang Huang <huan1562@purdue.edu>
2022-10-30 16:10:10 -0400

    Update preprocess

--- preprocess.sh
+++ preprocess.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 python src/preprocess.py \
     --in_dir 'dataset/raw/' \
     --out_dir 'dataset/processed/' \
     --chunk_time 5
 
 # list all folders in the processed folder except spectrogram
-for folder in $(ls -d dataset/processed/* | grep -v spectrogram); do
+for folder in $(ls -d dataset/processed/* | grep -v ZOOM0009); do
     for wav in $(ls $folder/*.wav); do
         # echo $folder/spectrogram
         # run in parallel subprocesses
@@ -12,5 +13,30 @@ for folder in $(ls -d dataset/processed/* | grep -v spectrogram); do
         python src/wav_to_spectrogram.py \
             --in_file $wav \
             --out_dir $folder/spectrogram/ 
+    done
+done
+
+# copy <dirname>_split<n>/spectrogram/* to dataset/processed/<dirname>/spectrogram/*
+
+# echo $(ls -d dataset/processed/*_split* | grep -v ZOOM0009)
+# press any key to continue
+
+
+for folder in $(ls -d dataset/processed/*_split* | grep -v ZOOM0009); do
+
+    target_dir=$(echo $folder | sed 's/_split.*//g')
+    if [ ! -d $target_dir/ ]; then
+        mkdir $target_dir/
+    fi
+
+    if [ ! -f $target_dir/spectrogram/ ]; then
+        mkdir $target_dir/spectrogram/
+    fi
+
+    for spectrogram in $(ls $folder/spectrogram/*); do
+
+        # echo $(echo $folder | sed 's/_split.*//g')/spectrogram/
+
+        cp $spectrogram $target_dir/spectrogram/
     done
 done
