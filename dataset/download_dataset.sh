b8605a009bcc326a7f8323293172fee5f29741dd
Ziyang Huang <35769263+sheepy928@users.noreply.github.com>
2022-11-02 15:44:19 -0400

    Update download_dataset.sh

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,4 +1,6 @@
 # Download the dataset
 echo "Downloading the dataset..."
-gdown 1McvBVNhXeazNnDKG-TD3ohbJhB8ugF20
-gdown 1dJ2Vw-W_5iZkZK6XySULEdfEf0gzydNd
+# gdown 1McvBVNhXeazNnDKG-TD3ohbJhB8ugF20
+# gdown 1dJ2Vw-W_5iZkZK6XySULEdfEf0gzydNd
+gdown 19KOBnVmQLw68Nadt4fHqiaDxknTVFE8w
+gdown 1AoFwr4eUfPlojPN-xks9qM2p19oenAib
