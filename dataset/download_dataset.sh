6a241204e58a006681a095969a035795cb346bde
Ziyang Huang <sheepy928@gmail.com>
2022-11-03 12:54:05 -0400

    Fix dataset downloading

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -2,5 +2,7 @@
 echo "Downloading the dataset..."
 # gdown 1McvBVNhXeazNnDKG-TD3ohbJhB8ugF20
 # gdown 1dJ2Vw-W_5iZkZK6XySULEdfEf0gzydNd
-gdown 19KOBnVmQLw68Nadt4fHqiaDxknTVFE8w
-gdown 1AoFwr4eUfPlojPN-xks9qM2p19oenAib
+# gdown 19KOBnVmQLw68Nadt4fHqiaDxknTVFE8w
+gdown https://drive.google.com/u/0/uc?id=19KOBnVmQLw68Nadt4fHqiaDxknTVFE8w&export=download&confirm=t
+gdown https://drive.google.com/u/0/uc?id=1AoFwr4eUfPlojPN-xks9qM2p19oenAib&export=download&confirm=t
+# gdown 1AoFwr4eUfPlojPN-xks9qM2p19oenAib
