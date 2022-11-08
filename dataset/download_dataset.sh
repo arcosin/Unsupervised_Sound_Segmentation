e333cf279da3c1fd4e894fb6adb2b9ef29cedae4
Ziyang Huang <sheepy928@gmail.com>
2022-11-07 22:26:44 -0500

    Fixed gdown links

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,8 +1,15 @@
 # Download the dataset
 echo "Downloading the dataset..."
+
+# Original dataset
 # gdown 1McvBVNhXeazNnDKG-TD3ohbJhB8ugF20
 # gdown 1dJ2Vw-W_5iZkZK6XySULEdfEf0gzydNd
+# gdown https://drive.google.com/u/0/uc?id=1McvBVNhXeazNnDKG-TD3ohbJhB8ugF20&export=download&confirm=t
+# gdown https://drive.google.com/u/0/uc?id=1dJ2Vw-W_5iZkZK6XySULEdfEf0gzydNd&export=download&confirm=t
+
+# New dataset
 # gdown 19KOBnVmQLw68Nadt4fHqiaDxknTVFE8w
+# gdown 1AoFwr4eUfPlojPN-xks9qM2p19oenAib
 gdown https://drive.google.com/u/0/uc?id=19KOBnVmQLw68Nadt4fHqiaDxknTVFE8w&export=download&confirm=t
 gdown https://drive.google.com/u/0/uc?id=1AoFwr4eUfPlojPN-xks9qM2p19oenAib&export=download&confirm=t
-# gdown 1AoFwr4eUfPlojPN-xks9qM2p19oenAib
+
